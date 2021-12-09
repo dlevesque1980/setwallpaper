07df2369b38fca8bb0e9eeb57114a254f911a60c
Didier Levesque <didier.levesque@gmail.com>
2021-12-09 06:27:21 -0500

    upgrade version values

--- lib/setwallpaper.dart
+++ lib/setwallpaper.dart
@@ -6,18 +6,18 @@ class Setwallpaper {
   static const MethodChannel _channel =
   const MethodChannel('didisoft.wallpaper');
 
-  static Future<String> setSystemWallpaper(String url) async {
-    final String message = await _channel.invokeMethod('setsystemwallpaper',{'url': url, 'system': true, 'locked': false});
+  static Future<String?> setSystemWallpaper(String url) async {
+    final String? message = await _channel.invokeMethod('setsystemwallpaper',{'url': url, 'system': true, 'locked': false});
     return message;
   }
 
-  static Future<String> setLockedWallpaper(String url) async {
-    final String message = await _channel.invokeMethod('setlockedwallpaper',{'url': url, 'system': false, 'locked': true});
+  static Future<String?> setLockedWallpaper(String url) async {
+    final String? message = await _channel.invokeMethod('setlockedwallpaper',{'url': url, 'system': false, 'locked': true});
     return message;
   }
 
-  static Future<String> setBothWallpaper(String url) async {
-    final String message = await _channel.invokeMethod('setbothwallpaper',{'url': url, 'system': true, 'locked': true});
+  static Future<String?> setBothWallpaper(String url) async {
+    final String? message = await _channel.invokeMethod('setbothwallpaper',{'url': url, 'system': true, 'locked': true});
     return message;
   }
 }
